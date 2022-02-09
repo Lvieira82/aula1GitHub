df136805e5dc544841b4f3af1061140814215140
Laerte Vieira <lvieiraoficial@gmail.com>
2022-02-09 09:57:56 -0300

    laço de contagem

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -3,8 +3,10 @@ package aplication;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("bom dia");
+		for (int i = 0; i<10; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
